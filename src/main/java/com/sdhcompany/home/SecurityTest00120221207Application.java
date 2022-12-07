b99864be6887049163dcc846970a61a5fb605f69
ici <ici@DESKTOP-8CT0OFV>
2022-12-07 17:33:36 +0900

    설정 진행중

---
+++ src/main/java/com/sdhcompany/home/SecurityTest00120221207Application.java
@@ -0,0 +1,13 @@
+package com.sdhcompany.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTest00120221207Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTest00120221207Application.class, args);
+	}
+
+}
